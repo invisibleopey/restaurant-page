778ac9dc16c9953dd594eec1c493c32a3e097a2a
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-07-05 17:54:46 +0100

    Dynamically added the nav, hero and brand image

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('All is working well');
+import BrandImg from './seafood-okra-soup.jpg'
+
+const content = document.querySelector('#content');
+// Navigation
+const nav = document.createElement('ul');
+const home = document.createElement('li');
+const menu = document.createElement('li');
+const contact = document.createElement('li');
+home.textContent = 'Home';
+menu.textContent = 'Menu';
+contact.textContent = "Contact";
+nav.append(home, menu, contact);
+// Headline Text
+const hero = document.createElement('h1');
+hero.textContent = 'Invisible Afro Cuisines';
+// Brand Image Addition
+const myBrandImg = new Image();
+myBrandImg.src = BrandImg;
+
+content.append(nav, hero, myBrandImg);
