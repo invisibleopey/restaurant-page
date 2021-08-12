fdfb0d2560a9e60aa7fa8d5c5135be037ca85a43
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-08-12 23:59:19 +0100

    Added two of the menu items

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,5 @@
+import OkroImg from './seafood-okra-soup.jpg';
+import EgusiImg from './egusi-soup.jpg';
 import './style.css';
 
 function loadMenuPage() {
@@ -17,9 +19,24 @@ function loadMenuPage() {
     nav.append(home, menu, contact);
     // Menu Content
     const container = document.createElement('div');
+    container.classList.add('menuDiv');
+    // First Item
     const box1 = document.createElement('div');
+    const myOkroImg = new Image();
+    myOkroImg.src = OkroImg;
+    const item1 = document.createElement('h2');
+    item1.textContent = `Seafood Okro Soup`;
+    box1.append(myOkroImg, item1);
+    // Second Item
     const box2 = document.createElement('div');
+    const myEgusiImg = new Image();
+    myEgusiImg.src = EgusiImg;
+    const item2 = document.createElement('h2');
+    item2.textContent = `Egusi Soup`;
+    box2.append(myEgusiImg, item2);
+    // Third Item
     const box3 = document.createElement('div');
+    // Fourth Item
     const box4 = document.createElement('div');
     container.append(box1, box2, box3, box4);
     content.append(nav, container);
